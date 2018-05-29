954c5392c5c4473f6077d61b3117c178a2968fc2
Jenny <jenny@playgroundinc.com>
2018-05-29 12:03:14 -0400

    Check for mentions

--- index.js
+++ index.js
@@ -10,18 +10,19 @@ rtm.on('message', (event) => {
 
     console.log(event);
 
-    if (event.type === 'message' || event.subtype != 'message_deleted') {
+    if (event.type === 'message' && event.text) {
         // Skip messages that are from a bot or my own user ID
         if ((event.subtype && event.subtype === 'bot_event') ||
             (!event.subtype && event.user === rtm.activeUserId)) {
             return;
         }
+        
         const message = event;
         const listen = ['mel', 'mel?', 'mel\'', 'mel!'];
         const text = (message.text).toLowerCase().split(' ');
         const response = responses[Math.floor(Math.random() * responses.length)];
        
-        if (text.some(word => listen.includes(word))) {
+        if (text.some(word => listen.includes(word)) || message.text.includes(`<@${rtm.activeUserId}>`)) {
             rtm.sendMessage(response, message.channel)
                 .then(msg => console.log('Sent!'))
                 .catch(err => console.log('PROBLEM: ', err));
